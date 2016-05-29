5acbad742dd950d6c34bd9220b87e3723f3b7c9a
15991893800 <15991893800@163.com>
2016-05-29 23:19:49 +0800

    commit ContentUtil

---
+++ Merchant's_Guide/src/com/yk/marchant/test/ContentUtilTest.java
@@ -0,0 +1,34 @@
+package com.yk.marchant.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.yk.marchant.util.ContentUtil;
+
+public class ContentUtilTest {
+
+	// Test Content util
+	@Test
+	public void testContentUtil() {
+		
+		String isUtil = ContentUtil.IS_UTIL;
+		assertEquals(isUtil, " is ");
+		
+		String spaceUtil = ContentUtil.SPACE_UTIL;
+		assertEquals(spaceUtil, " ");
+		
+		String questionMarkUtil = ContentUtil.QUESTION_MARK_UTIL;
+		assertEquals(questionMarkUtil, " ?");
+		
+		String howManyUtil = ContentUtil.HOW_MANY_UTIL;
+		assertEquals(howManyUtil, "how many");
+		
+		String howMuchUtil = ContentUtil.HOW_MUCH_UTIL;
+		assertEquals(howMuchUtil, "how much");
+		
+		String creditsUtil = ContentUtil.CREDITS_UTIL;
+		assertEquals(creditsUtil, "Credits");
+	}
+
+}
---
+++ Merchant's_Guide/src/com/yk/marchant/util/ContentUtil.java
@@ -0,0 +1,17 @@
+package com.yk.marchant.util;
+
+// Define content split util
+public class ContentUtil {
+	
+	public static final String IS_UTIL = " is ";
+
+	public static final String SPACE_UTIL = " ";
+	
+	public static final String QUESTION_MARK_UTIL = " ?";
+
+	public static final String HOW_MANY_UTIL = "how many";
+
+	public static final String HOW_MUCH_UTIL = "how much";
+
+	public static final String CREDITS_UTIL = "Credits";
+}
