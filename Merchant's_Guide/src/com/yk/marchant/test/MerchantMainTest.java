617bbfcb8517ed91be1a7adb51f8c371712ae70b
15991893800 <15991893800@163.com>
2016-05-29 18:19:02 +0800

    add MerchantMainTest

---
+++ Merchant's_Guide/src/com/yk/marchant/test/MerchantMainTest.java
@@ -0,0 +1,14 @@
+package com.yk.marchant.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MerchantMainTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
