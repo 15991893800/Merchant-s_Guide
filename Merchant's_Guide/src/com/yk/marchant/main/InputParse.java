e83fda7c8ffe35c60cc7cf174d6f5472c3af4ad5
15991893800 <15991893800@163.com>
2016-05-30 09:14:59 +0800

    commit

--- Merchant's_Guide/src/com/yk/marchant/main/InputParse.java
+++ Merchant's_Guide/src/com/yk/marchant/main/InputParse.java
@@ -3,6 +3,7 @@ package com.yk.marchant.main;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class InputParse {
 	
@@ -26,5 +27,16 @@ public class InputParse {
 		return new BufferedReader(new FileReader("D:\\SampleInput.txt"));
 	}
     
-    
+	// Parse input File
+    public void parseInputFile() throws IOException{
+    	BufferedReader br = getInput();
+    	String currentLine;
+    	OutputParse outputParse = new OutputParse();
+    	while((currentLine = br.readLine()) != null){
+    		outputParse.parseCurrentLine(currentLine);
+    	}
+    	
+    	//call the output mothod of OutputParse, output the result
+    	outputParse.getOutput();
+    }
 }
---
+++ Merchant's_Guide/src/com/yk/marchant/main/OutputParse.java
@@ -0,0 +1,23 @@
+package com.yk.marchant.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutputParse {
+
+	//Define output list
+	private List<String> outPutList = new ArrayList<String>();
+	
+	//Parse the input line and add output content
+	public void parseCurrentLine(String currentLine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//Output method
+	public void getOutput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
